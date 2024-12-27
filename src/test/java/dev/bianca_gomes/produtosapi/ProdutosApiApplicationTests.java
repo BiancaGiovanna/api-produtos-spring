e45979f2f66b979efd59f0fbf229f5759e0d5406
BiancaGiovanna <biancagothicg@gmail.com>
2024-12-26 21:36:40 -0300

    First Commit

---
+++ src/test/java/dev/bianca_gomes/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bianca_gomes.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
