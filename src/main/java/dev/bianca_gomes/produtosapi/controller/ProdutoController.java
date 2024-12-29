c020f75fc1d6f4922b20e7ba8842f1fae24a91a0
BiancaGiovanna <biancagothicg@gmail.com>
2024-12-28 21:41:27 -0300

    Criando endpoint para Receber o Produto

---
+++ src/main/java/dev/bianca_gomes/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,18 @@
+package dev.bianca_gomes.produtosapi.controller;
+
+import dev.bianca_gomes.produtosapi.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    @PostMapping
+    public Produto salvarProduto(@RequestBody Produto produto) {
+        System.out.println("Produto recebido "+ produto);
+        return produto;
+    }
+}
