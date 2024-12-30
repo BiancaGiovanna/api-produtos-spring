63b8a6745c394fcfa441a35bff8c64a46de25b99
BiancaGiovanna <biancagothicg@gmail.com>
2024-12-30 14:11:22 -0300

    criando mapeamento produto

--- src/main/java/dev/bianca_gomes/produtosapi/controller/ProdutoController.java
+++ src/main/java/dev/bianca_gomes/produtosapi/controller/ProdutoController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
